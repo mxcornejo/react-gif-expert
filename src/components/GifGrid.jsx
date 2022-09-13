e6064bf15845dc03955b477668615843ea906920
mcornejoa <mcornejoa.2brains@bancochile.cl>
2022-09-13 14:49:51 -0300

    archivos finales del curso

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,3 +1,5 @@
+import { PropTypes } from "prop-types";
+
 import { GifItem } from './GifItem';
 import { useFetchGifs } from '../hooks/useFetchGifs';
 
@@ -9,7 +11,7 @@ export const GifGrid = ({ category }) => {
         <>
             <h3>{category}</h3>
             {
-                isLoading && (<h2>Cargando ...</h2>)
+                isLoading && (<h2>Cargando...</h2>)
             }
 
             <div className='card-grid'>
@@ -24,4 +26,8 @@ export const GifGrid = ({ category }) => {
             </div>
         </>
     )
+}
+
+GifGrid.propTypes = {
+    category: PropTypes.string.isRequired,
 }
